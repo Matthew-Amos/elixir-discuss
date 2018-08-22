244cd3b2a72a8df0b1ad029c3be6678d2dbba2d4
equinaut <matthewamos@live.com>
2018-08-22 07:28:28 -0400

    completed most of the /topic modules

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,16 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  # Note that params maps are string: value
+  def create(conn, %{"topic" => topic}) do
+
+  end
+end
