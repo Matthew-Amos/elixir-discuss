12ef71d2f827080e5f32f781a69ec40e75b3d735
equinaut <matthewamos@live.com>
2018-08-31 06:13:24 -0400

    implemented /edit

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -18,15 +18,41 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, topic} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: topic_path(conn, :index))
       {:error, changeset} ->
         conn
         |> put_flash(:error, "Invalid Submission")
-        |> render "new.html", changeset: changeset
+        |> render("new.html", changeset: changeset)
     end
 
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    # Pull a single record from Topic with topic_id
+    topic = Repo.get(Topic, topic_id)
+
+    # Create a changeset that looks like `topic`
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Invalid Submission")
+        |> render("edit.html", changeset: changeset, topic: old_topic)
+    end
   end
 end
